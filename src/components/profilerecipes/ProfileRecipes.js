dfe135c8543f87745aaf92c6edc19a6a2d985a37
unknown <ben@benstephan.com>
2020-11-30 09:35:02 -0500

    Profile page updates

--- src/components/profilerecipes/ProfileRecipes.js
+++ src/components/profilerecipes/ProfileRecipes.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import RecipeCard from '../recipecard/RecipeCard';
+import './profilerecipes.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes, faPencilAlt } from '@fortawesome/free-solid-svg-icons';
 
 export class ProfileRecipes extends React.Component {
     state = {
@@ -7,7 +9,7 @@ export class ProfileRecipes extends React.Component {
     }
     componentDidMount() {
         const authorName = localStorage.getItem('name');
-        fetch(`http://localhost:4000/getMyRecipes?author=${authorName}`)
+        fetch(`http://localhost:4000/getData`)
         .then(res => res.json())
         .then((res) => {
             this.setState({ recipes: res })
@@ -15,11 +17,23 @@ export class ProfileRecipes extends React.Component {
 
         .catch(console.log)
     }
+
+    
     
     render() {
+        let loginID = localStorage.getItem('loggedin');
         return (
             <div className="profile-recipe-list">
-                <RecipeCard recipes={this.state.recipes} />
+                {this.state.recipes.filter(recipe => recipe.author===loginID).map((recipe, i) => (
+                    <div className="profile-recipe-list__card" key={i}>
+                        <div className="profile-recipe-list__image"><img src={recipe.imageURL} alt={recipe.name} /></div>
+                        <div className="profile-recipe-list__recipe-title">{recipe.name}</div>
+                        <div className="profile-recipe-list__edit-buttons">
+                            <button class="btn btn-danger"><FontAwesomeIcon icon={faTimes} /></button>
+                            <button class="btn btn-success"><FontAwesomeIcon icon={faPencilAlt} /></button>
+                        </div>
+                    </div>
+                ))}
             </div>
             
         )
